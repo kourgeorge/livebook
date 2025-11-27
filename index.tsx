364b58176a290dcb88bf22ece4b14043a4fcb32e
georgekour <gkour@ibm.com>
2025-11-27 11:18:45 +0200

    fix the urls

--- index.tsx
+++ index.tsx
@@ -11,7 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/livebook">
       <App />
     </BrowserRouter>
   </React.StrictMode>
